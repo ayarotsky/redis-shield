0cd42c9b304c90cfc8cd7a753b976f83fd3812fd
Alex Yarotsky <yarotsky.alex@gmail.com>
2020-01-25 17:02:57 +0300

    Add integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -48,8 +48,174 @@ redis_module! {
 
 #[cfg(test)]
 mod tests {
+    extern crate redis;
+    use redis::Commands;
+    use std::env;
+    use std::{thread, time};
+
+    fn establish_connection() -> redis::Connection {
+        let redis_url = env::var("REDIS_URL").unwrap();
+        let client = redis::Client::open(redis_url).unwrap();
+        client.get_connection().unwrap()
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_when_bucket_does_not_exist() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_new".to_string();
+
+        let _: () = con.del(&bucket_key).unwrap();
+
+        let remaining_tokens: i64 = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(30)
+            .arg(60)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 29);
+
+        let ttl: i64 = con.pttl(&bucket_key).unwrap();
+        assert!(ttl >= 59900 && ttl <= 60000);
+    }
+
+    #[test]
+    fn test_when_bucket_exist_but_has_no_associated_expire() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_no_expire".to_string();
+
+        let _: () = con.del(&bucket_key).unwrap();
+        let _: () = con.set(&bucket_key, 2).unwrap();
+
+        let remaining_tokens: i64 = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(30)
+            .arg(60)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 29);
+
+        let ttl: i64 = con.pttl(&bucket_key).unwrap();
+        assert!(ttl >= 59900 && ttl <= 60000);
+    }
+
+    #[test]
+    fn test_when_multiple_tokens_requested() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_multiple_tokens".to_string();
+
+        let _: () = con.del(&bucket_key).unwrap();
+
+        let remaining_tokens: i64 = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(30)
+            .arg(60)
+            .arg(25)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 5);
+    }
+
+    #[test]
+    #[should_panic(expected = "An error was signalled by the server: bucket is overflown")]
+    fn test_when_bucket_is_overflown() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_overflown".to_string();
+
+        let _: () = con.del(&bucket_key).unwrap();
+
+        let _: () = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(30)
+            .arg(60)
+            .arg(31)
+            .query(&mut con)
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "An error was signalled by the server: bucket is overflown")]
+    fn test_sequential_requests() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_sequential_requests".to_string();
+        let tokens = 2;
+        let period = 60;
+
+        let _: () = con.del(&bucket_key).unwrap();
+
+        let mut remaining_tokens: i64 = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 1);
+
+        let mut ttl: i64 = con.pttl(&bucket_key).unwrap();
+        assert!(ttl >= 59900 && ttl <= 60000);
+
+        remaining_tokens = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 0);
+
+        ttl = con.pttl(&bucket_key).unwrap();
+        assert!(ttl >= 59900 && ttl <= 60000);
+
+        let _: () = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+    }
+
+    #[test]
+    fn test_bucket_refills_with_time() {
+        let mut con = establish_connection();
+        let bucket_key = "redis-shield::test_key_refill".to_string();
+        let tokens = 3;
+        let period = 6;
+
+        let _: () = con.del(&bucket_key).unwrap();
+
+        let mut remaining_tokens: i64 = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 2);
+
+        thread::sleep(time::Duration::from_secs(period / 3 + 1));
+
+        remaining_tokens = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 2);
+
+        remaining_tokens = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .arg(2)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 0);
+
+        thread::sleep(time::Duration::from_secs(6));
+
+        remaining_tokens = redis::cmd("SHIELD.absorb")
+            .arg(&bucket_key)
+            .arg(tokens)
+            .arg(period)
+            .query(&mut con)
+            .unwrap();
+        assert_eq!(remaining_tokens, 2);
     }
 }
