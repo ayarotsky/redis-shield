f5406e5298e29affd8346afd90d6c1d663577014
dmytron <dmitry.neroba@gmail.com>
2025-12-18 15:55:00 +0200

    feat(add-more-algo):  Added LeakyBucket
    Organizaed project structure, putting algos into crate

--- src/traffic_policy.rs
+++ src/traffic_policy.rs
@@ -1,6 +1,5 @@
+use crate::algorithm::{LeakyBucket, TokenBucket};
 use redis_module::{Context, RedisError, RedisString};
-mod leaky_bucket_lua;
-mod token_bucket;
 
 const TRAFFIC_POLICY_KEY_PREFIX: &str = "tp";
 
@@ -14,7 +13,7 @@ trait TrafficPolicySuffix {
 
 pub enum PolicyConfig {
     TokenBucket { capacity: i64, period: i64 },
-    // LeakyBucket { capacity: i64, period: i64 },
+    LeakyBucket { capacity: i64, period: i64 },
     // FixedWindow { capacity: i64, window: i64 },
     // SlidingWindow { capacity: i64, window: i64 },
 }
@@ -23,7 +22,7 @@ impl TrafficPolicySuffix for PolicyConfig {
     fn suffix(&self) -> &'static str {
         match self {
             PolicyConfig::TokenBucket { .. } => "tb",
-            // PolicyConfig::LeakyBucket { .. } => "lb",
+            PolicyConfig::LeakyBucket { .. } => "lb",
             // PolicyConfig::FixedWindow { .. } => "fw",
             // PolicyConfig::SlidingWindow { .. } => "sw",
         }
@@ -41,18 +40,17 @@ pub fn create_executor<'a>(
     ));
     let key_ref: &'a RedisString = Box::leak(owned_key);
     match *cfg {
-        PolicyConfig::TokenBucket { capacity, period } => Ok(Box::new(
-            token_bucket::TokenBucket::new(ctx, key_ref, capacity, period)?,
-        )),
-        // PolicyConfig::LeakyBucket { capacity, period } => Ok(Box::new(
-        //     leaky_bucket_lua::LeakyBucket::new(ctx, key_ref, capacity, period)?,
-        // )),
-        // PolicyConfig::FixedWindow { capacity, window } => {
-        //     Ok(Box::new(FixedWindow::new(ctx, key, max_hits, window)?))
-        // }
-        // PolicyConfig::SlidingWindow { capccity, window } => {
-        //     Ok(Box::new(SlidingWindow::new(ctx, key, max_hits, window)?))
-        // }
+        PolicyConfig::TokenBucket { capacity, period } => {
+            Ok(Box::new(TokenBucket::new(ctx, key_ref, capacity, period)?))
+        }
+        PolicyConfig::LeakyBucket { capacity, period } => {
+            Ok(Box::new(LeakyBucket::new(ctx, key_ref, capacity, period)?))
+        } // PolicyConfig::FixedWindow { capacity, window } => {
+          //     Ok(Box::new(FixedWindow::new(ctx, key, max_hits, window)?))
+          // }
+          // PolicyConfig::SlidingWindow { capccity, window } => {
+          //     Ok(Box::new(SlidingWindow::new(ctx, key, max_hits, window)?))
+          // }
     }
 }
 
